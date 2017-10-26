0a2d383ce4a5060b93bc18b3e98c4ec2cbfd54df
Artem Yavorsky <artem@yavorsky.org>
2017-10-26 22:39:20 +0300

    Better tooltip styles merge

--- log.py
+++ log.py
@@ -4,14 +4,20 @@ from . import defaults
 from . import Tooltip
 
 def show_tooltip(view, package, version):
-    tooltip_styles = conf.settings.get('tooltip_styles', defaults.get_tooltip_styles())
-    view.show_popup(Tooltip.get_str().substitute(package=package, version=version, package_color=tooltip_styles['package_color'], version_color=tooltip_styles['version_color'], background=tooltip_styles['background']), location=-1, max_width=400)
+    common_styles = defaults.get_tooltip_styles()
+    tooltip_styles = conf.settings.get('tooltip_styles', {})
+
+    for field in tooltip_styles:
+      if field in common_styles:
+          common_styles[field] = tooltip_styles[field]
+
+    view.show_popup(Tooltip.get_str().substitute(package=package, version=version, package_color=common_styles['package_color'], version_color=common_styles['version_color'], background=common_styles['background']), location=-1, max_width=400)
 
 def log_version(view, package, version, tooltip = False):
     formatted = Template('$package: $version').substitute(package=package, version=version)
     update_log(view, formatted);
     if tooltip:
-      show_tooltip(view, package, version)
+        show_tooltip(view, package, version)
 
 def update_log(view, text):
     unset_log(view)
