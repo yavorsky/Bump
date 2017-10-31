21b789a87f380b26a046fcb29fd99a58a1f8199b
Artem Yavorsky <artem@yavorsky.org>
2017-10-31 13:40:53 +0200

    Tooltip: Log satisfied package as a link

--- Tooltip.py
+++ Tooltip.py
@@ -4,4 +4,8 @@ def get_str(has_matched):
     matched_icon = ''
     if has_matched:
       matched_icon = '<span class="check-icon">✓</span>'
-    return Template('<style>html { background-color: $background; margin: 0px; } body { margin: 5px; } div { margin: 0px; word-wrap:break-word; } .line { margin: 4px 5px; font-size: 13px; } .package { color: $package_color; } .version { color: $version_color } .check-icon { color: #B9DE82; padding-left: 5px; }</style><div><p class="line"><span class="package">$package: </span><span class="version"> $version</span>' + matched_icon + '</p></div>')
+      matched_version = '<span class="version"> $version</span>'
+    else:
+      matched_version = '<a href="" class="version"> $version</a>'
+
+    return Template('<style>html { background-color: $background; margin: 0px; } body { margin: 5px; } div { margin: 0px; word-wrap:break-word; } .line { margin: 4px 5px; font-size: 13px; } .package { color: $package_color; } .version { color: $version_color; text-decoration: none; } .check-icon { color: #B9DE82; padding-left: 5px; }</style><div><p class="line"><span class="package">$package: </span>' + matched_version + matched_icon + '</p></div>')
