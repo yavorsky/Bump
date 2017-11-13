f8a441e3670e51f3af2749cef0cc099ec497dd0e
Artem Yavorsky <artem@yavorsky.org>
2017-11-13 12:05:09 +0200

    Fix request path for scoped packages.

--- request.py
+++ request.py
@@ -14,7 +14,7 @@ def get_request(pathname):
 
 def fetch_package_version(package, version, callback = None):
     registry = conf.settings.get('registry', defaults.get_registry())
-    pathname = os.path.join(registry, package)
+    pathname = os.path.join(registry, package.replace('/', '%2F'))
     pathname = Template('$pathname?version=$version').substitute(pathname=pathname, version=version)
     response = get_request(pathname)
     if (callback):
